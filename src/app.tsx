3595b501c9a52a19794a4b51738a0a4bff53b525
Sebastián Suárez Valencia <>
2024-11-12 17:16:09 -0500

    Add GitHooks

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,5 @@
 /* eslint-disable unicorn/filename-case */
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Container from "@mui/material/Container";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
 import routes from "./pages/routes.js";
 
 const router = createBrowserRouter(routes);
